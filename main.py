76e310a4cddcbdb65ab467a7e2988abe103b8ab3
p312734 <34969190+p312734@users.noreply.github.com>
2025-02-10 22:09:45 +0100

    zfill not working - before switch to JavaScript

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 def on_logo_pressed():
     basic.show_string("L")
+    
     OLED.write_string_new_line(
-        RTC_DS1307.get_time(RTC_DS1307.TimeType.YEAR) + "-" 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MONTH) + "-" 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.DAY) + "-" 
-        + " " 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR) + ":" 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) + ":" 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) + ":" 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.SECOND))
-
+        str(RTC_DS1307.get_time(RTC_DS1307.TimeType.YEAR)) + "-"
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MONTH)).zfill(2) + "-"
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.DAY)).zfill(2) + " "
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR)).zfill(2) + ":"
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE)).zfill(2) + ":"
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.SECOND)).zfill(2))
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
 def on_button_pressed_a():
@@ -17,37 +15,27 @@ def on_button_pressed_a():
     OLED.write_string_new_line("Knopf A")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    basic.show_string("B")
-    OLED.write_string_new_line("Knopf B")
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
 def on_button_pressed_ab():
     OLED.clear()
     RTC_DS1307.set_time(RTC_DS1307.TimeType.YEAR, 2025)
     RTC_DS1307.set_time(RTC_DS1307.TimeType.MONTH, 2)
     RTC_DS1307.set_time(RTC_DS1307.TimeType.DAY, 10)
-
     RTC_DS1307.set_time(RTC_DS1307.TimeType.HOUR, 21)
-    RTC_DS1307.set_time(RTC_DS1307.TimeType.MINUTE, 10)
+    RTC_DS1307.set_time(RTC_DS1307.TimeType.MINUTE, 20)
     RTC_DS1307.set_time(RTC_DS1307.TimeType.SECOND, 0)
-
     OLED.write_string_new_line(
-        "Uhrzeit gesetzt: " 
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.YEAR) + "-"
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MONTH) + "-"
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.DAY) + "-"
-        + " "
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR) + ":"
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) + ":"
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE) + ":"
-        + RTC_DS1307.get_time(RTC_DS1307.TimeType.SECOND))
+        "Uhrzeit gesetzt: "  
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.YEAR)) + "-" 
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MONTH)) + "-" 
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.DAY)) + " " 
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.HOUR)) + ":" 
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.MINUTE)) + ":"  
+        + str(RTC_DS1307.get_time(RTC_DS1307.TimeType.SECOND)))
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+def on_button_pressed_b():
+    basic.show_string("B")
+    OLED.write_string_new_line("Knopf B")
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
-
-
-
-
-
-
+OLED.init(128, 64)
--- main.ts
+++ main.ts
@@ -1,14 +1,10 @@
 input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
     basic.showString("L")
-    OLED.writeStringNewLine(RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR) + "-" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH) + "-" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY) + "-" + " " + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND))
+    OLED.writeStringNewLine("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR) + "-" + _py.py_string_zfill("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH), 2) + "-" + _py.py_string_zfill("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY), 2) + " " + _py.py_string_zfill("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR), 2) + ":" + _py.py_string_zfill("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE), 2) + ":" + _py.py_string_zfill("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND), 2))
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     basic.showString("A")
     OLED.writeStringNewLine("Knopf A")
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    basic.showString("B")
-    OLED.writeStringNewLine("Knopf B")
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     OLED.clear()
@@ -16,7 +12,12 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     RTC_DS1307.setTime(RTC_DS1307.TimeType.MONTH, 2)
     RTC_DS1307.setTime(RTC_DS1307.TimeType.DAY, 10)
     RTC_DS1307.setTime(RTC_DS1307.TimeType.HOUR, 21)
-    RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, 10)
+    RTC_DS1307.setTime(RTC_DS1307.TimeType.MINUTE, 20)
     RTC_DS1307.setTime(RTC_DS1307.TimeType.SECOND, 0)
-    OLED.writeStringNewLine("Uhrzeit gesetzt: " + RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR) + "-" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH) + "-" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY) + "-" + " " + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE) + ":" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND))
+    OLED.writeStringNewLine("Uhrzeit gesetzt: " + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.YEAR)) + "-" + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MONTH)) + "-" + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.DAY)) + " " + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.HOUR)) + ":" + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.MINUTE)) + ":" + ("" + RTC_DS1307.getTime(RTC_DS1307.TimeType.SECOND)))
 })
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    basic.showString("B")
+    OLED.writeStringNewLine("Knopf B")
+})
+OLED.init(128, 64)
